902057d58cc066e81da4a6b42fc44557a4a7c1e9
Filip Weidemann <filip.weidemann@lfda.de>
2024-07-29 13:21:59 +0200

    feat: add reverse name for search view

--- djangocms_haystack/urls.py
+++ djangocms_haystack/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from djangocms_haystack.views import SearchView
 
-urlpatterns = [path("", SearchView.as_view())]
+urlpatterns = [
+    path("", SearchView.as_view(), name="djangocms-haystack-search-view")
+]
