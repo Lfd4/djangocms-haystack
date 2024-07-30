23d54d213d126e69bdcf6721bf17d05f33ac7812
Filip Weidemann <filip.weidemann@lfda.de>
2024-07-30 18:05:10 +0200

    feat: execute tests in ci

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,15 @@ import nox
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("ruff")
-    session.run("ruff", "check")
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("poetry", "run", "ruff", "check")
+
+
+@nox.session
+def test(session: nox.Session) -> None:
+    session.install("poetry")
+    session.run("poetry", "install")
+
+    cmd = "poetry run django-app-helper djangocms_haystack test --cms --extra-settings tests/settings.py"
+    session.run(*cmd.split(" "))
